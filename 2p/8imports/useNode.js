9fdf4790ec05ac59158d8169fea8442bab6c790e
PRECIADO MARQUEZ MARQUEZ <115325002+hugopm123@users.noreply.github.com>
2022-10-19 22:50:20 +0000

    nn

---
+++ 2p/8imports/useNode.js
@@ -0,0 +1,5 @@
+import Node from '../../../HRPM/linked/node.js'
+
+let n=new Node('b')
+
+console.log(n)
--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-//agregar 10e3 pares (llave:valor) a un objeto
-
-console.time("Tiempo")
-let objnum = {},k =0 
-
-for (x = 1; x <= 10e3; x++) {
-    key = 'key:' + Math.floor(Math.random() * 10e3);
-    objnum[key] = key;
-  k++
+const traverse=(n)=>{//es6
+    let c=n//iterator 
+    while(c){//exists
+      console.log(c.data)
+    	c=c.next
+    }
 }
-console.log(objnum)
-console.timeEnd("Tiempo")
-console.log("Pasos :", + k)
+
+traverse(n1)
